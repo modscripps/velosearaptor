971e3d776bf54a6402b616c61f9e75bbb0ea860e
Gunnar Voet <gvoet@ucsd.edu>
2021-05-12 13:26:13 -0700

    blacken & update pressure plot

--- gadcp/madcp.py
+++ gadcp/madcp.py
@@ -13,6 +13,7 @@ from pathlib import Path
 
 from pycurrents.adcp.rdiraw import extract_raw, Multiread
 from pycurrents.system import Bunch
+
 # from pycurrents.adcp.mcm_avg import MCM, Pingavg
 from gadcp.mcm_avg import MCM, Pingavg
 
@@ -57,16 +58,17 @@ def proc(infile, lon, lat, end_pc, end_adcp, n_ensembles=None):
 
     print("reading raw data...")
     # raw = io.read_raw_rdi(infile)
-    raw = m.read(varlist=['VariableLeader'])
-    raw.pressure =raw.VL['Pressure'] / 1000.0
-    raw.temperature =raw.VL['Temperature'] / 100.0
-    fig, ax = gv.plot.quickfig()
+    raw = m.read(varlist=["VariableLeader"])
+    raw.pressure = raw.VL["Pressure"] / 1000.0
+    raw.temperature = raw.VL["Temperature"] / 100.0
+    fig, ax = gv.plot.quickfig(fgs=(6, 2.5))
     ax.plot(raw.dday, raw.pressure)
     ax.invert_yaxis()
+    ax.set(xlabel='julian day', ylabel='pressure [dbar]')
     # find time away from surface
     ii = np.argwhere(raw.pressure > 50).squeeze()
     # raw.pressure.isel(time=ii).plot(ax=ax, color="r")
-    ax.plot(raw.dday[ii], raw.pressure[ii], color='r')
+    ax.plot(raw.dday[ii], raw.pressure[ii], color="r")
 
     print("extract subsurface ping range")
     i0, i1 = ii[0], ii[-1]
@@ -88,49 +90,50 @@ def proc(infile, lon, lat, end_pc, end_adcp, n_ensembles=None):
     outdir = "./"
     datadir = "./"
     fnamesdict = dict(adcp=[outfile])
-    driftparamsdict = dict(adcp=dict(end_pc=end_pc,
-                                     end_adcp=end_adcp,
-                                     start_dday=t0))
+    driftparamsdict = dict(adcp=dict(end_pc=end_pc, end_adcp=end_adcp, start_dday=t0))
     positionsdict = dict(adcp=(lon, lat))
 
-    editparams = dict(max_e=0.2,          # absolute max e
-                      max_e_deviation=2,  # max in terms of sigma
-                      min_correlation=64) # 64 is RDI standard
-    dgridparams = dict(dbot=1500, # int(self.p_median),
-                       dtop=100,  # 50,
-                       d_interval=16)
-    tgridparams = dict(dt_hours = 1.0,  #  1.0/4,
-                    t0 = 132,
-                    #t1 = t1,
-                    )
+    editparams = dict(
+        max_e=0.2,  # absolute max e
+        max_e_deviation=2,  # max in terms of sigma
+        min_correlation=64,
+    )  # 64 is RDI standard
+    dgridparams = dict(dbot=1500, dtop=100, d_interval=16)  # int(self.p_median),  # 50,
+    tgridparams = dict(
+        dt_hours=1.0,  #  1.0/4,
+        t0=132,
+        # t1 = t1,
+    )
 
-    print('time averaging and depth gridding')
+    print("time averaging and depth gridding")
     for key in fnamesdict.keys():
         mcm = MCM(fnamesdict[key], driftparamsdict[key], datadir=datadir)
-        pa = Pingavg(mcm, lonlat=positionsdict[key],
-                    dgridparams=dgridparams,
-                    tgridparams=tgridparams,
-                    editparams=editparams,
-                    )
+        pa = Pingavg(
+            mcm,
+            lonlat=positionsdict[key],
+            dgridparams=dgridparams,
+            tgridparams=tgridparams,
+            editparams=editparams,
+        )
         pa.average_ensembles()
-        npzname = key + '_hourly.npz'
+        npzname = key + "_hourly.npz"
         pa.save_npz(npzname, outdir=outdir)
 
     # load the generated file
     data = npz2nc(npzname)
 
-    return m, data
+    return m, mcm, data
 
 
 def tmpload(file):
     mfd_prefix = "__mfd__"
     mfm_prefix = "__mfm__"
     fz = open(file, "rb")
-    z = np.load(fz, mmap_mode=None, encoding='bytes', allow_pickle=True)
+    z = np.load(fz, mmap_mode=None, encoding="bytes", allow_pickle=True)
     out = Bunch()
     for k, v in z.items():
         if k.startswith(mfd_prefix):
-            kma = k[len(mfd_prefix):]
+            kma = k[len(mfd_prefix) :]
             kmm = mfm_prefix + kma
             out[kma] = np.ma.array(v, mask=z[kmm], copy=False)
         elif k.startswith(mfm_prefix):
@@ -164,14 +167,14 @@ def npz2nc(npzfile):
     # identify variables
     k = dat.keys()
     varsint = []
-    vars1d =  []
+    vars1d = []
     vars2d = []
     for ki in k:
         try:
             tmp = dat[ki].shape
-            if len(tmp)==1:
+            if len(tmp) == 1:
                 vars1d.append(ki)
-            elif len(tmp)==2:
+            elif len(tmp) == 2:
                 vars2d.append(ki)
         except:
             tmp = None
@@ -183,13 +186,14 @@ def npz2nc(npzfile):
     time = [base + datetime.timedelta(days=ti) for ti in dat.dday]
     adcptime = [np.datetime64(ti) for ti in time]
     # generate Dataset
-    out = xr.Dataset({'pg': (['z', 'time'], dat.pg.T)},
-                    coords={'time': (['time'], adcptime),
-                            'z': (['z'], dat.dep)})
+    out = xr.Dataset(
+        {"pg": (["z", "time"], dat.pg.T)},
+        coords={"time": (["time"], adcptime), "z": (["z"], dat.dep)},
+    )
     for vari in vars2d:
-        out[vari] = (['z', 'time'], dat[vari].T)
+        out[vari] = (["z", "time"], dat[vari].T)
     for vari in vars1d:
-        if vari not in ['dep', 'dday']:
-            out[vari] = (['time'], dat[vari])
+        if vari not in ["dep", "dday"]:
+            out[vari] = (["time"], dat[vari])
 
     return out
