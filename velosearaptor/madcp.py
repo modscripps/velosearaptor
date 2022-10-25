modscripps/velosearaptor

commit 34fdda9330f1a9935f9a027d104f52fee5f9741e

Read parameters from yaml file (#46)
Adds a new subclass `ProcessADCPyml` to `velosearaptor.madcp` that let's the user provide processing parameters via a yaml file. An example yaml file and notebook have been added.

velosearaptor/madcp.py
@@ -2,6 +2,16 @@
 # -*- coding: utf-8 -*-
 """Module velosearaptor.madcp with functions for moored ADCPs.
 
+### General Use
+Processing a raw ADCP file from a moored deployment is a two-step process.
+- Instantiate a processing object with `ProcessADCP` or `ProcessADCPyml`. The
+  former expects the path to the raw data and a number of dictionaries with
+  processing parameters as input. The latter reads a .yml file containing the
+  processing parameters. See the respective docstrings for more information.
+- Process raw pings and possibly run a ping-averaging method on the data.
+  Options here are `ProcessADCP.process_pings`,
+  `ProcessADCP.average_ensembles`, and `ProcessADCP.burst_average_ensembles`.
+
 ### Notes
 Some general notes for this module.
 
@@ -99,6 +109,8 @@ class ProcessADCP:
         Mark beam with bad data (zero based). Defaults to None.
     logdir : str, optional
         Log file directory. Defaults to `log/`.
+    verbose : bool, optional
+        Output more processing info to screen.
     magdec : float, optional
         Magnetic declination in degrees.
     pressure : xr.DataArray, optional
@@ -296,7 +308,8 @@ class ProcessADCP:
 
         """
         self.m = Multiread(self.files, sonar="wh", ibad=self.ibad)
-        # Make some more meta data realily available by reading a single ping from the raw data.
+        # Make some more meta data realily available by reading a single ping
+        # from the raw data.
         ping = self.m.read(start=0, stop=1)
         self.meta_data.Bin1Dist = ping.FL.Bin1Dist / 100.0
         self.meta_data.NCells = ping.FL.NCells
@@ -1582,3 +1595,22 @@ class ProcessADCP:
         binmask = self.raw.bin.data < 0
         binmask[indices] = True
         return binmask
+
+
+class ProcessADCPyml(ProcessADCP):
+    """Moored ADCP processing with parameters provided via .yml file.
+
+    An example parameter file is included at
+    [`notebooks/parameters.yml`](https://github.com/modscripps/velosearaptor/tree/main/notebooks/parameters.yml)"""
+
+    def __init__(self, parameter_file, mooring, sn, **kwargs):
+        p = io.parse_yaml_input(parameter_file, "MAVS2", 24606)
+        super().__init__(
+            p["data_dir"],
+            meta_data=p["meta_data"],
+            driftparams=p["driftparams"],
+            tgridparams=p["tgridparams"],
+            dgridparams=p["dgridparams"],
+            editparams=p["editparams"],
+            **kwargs,
+        )

notebooks/parameters.yml
@@ -1,0 +1,64 @@
+# Project-wide meta-data
+meta_data:
+  project: BLT
+
+# Project-wide processing parameters
+processing_parameters:
+  tgridparams:
+    burst_average: True
+  dgridparams:
+  editparams:
+    min_correlation: 64
+    max_e: 0.3
+    pg_limit: 50
+
+# Mooring- and instrument-specific parameters.
+# Several moorings can be added.
+mooring:
+  MAVS2:
+    lon: -11.843
+    lat: 54.182
+    # List all ADCPs on this mooring here.
+    # Any processing parameters added here will overwrite project-wide
+    # processing parameters. 
+    # Meta-data added for specific instruments will be joined with project-wide
+    # meta-data.
+    SN24606:
+      driftparams:
+        end_adcp: [2021, 10, 7, 12, 7, 12]
+        end_pc: [2021, 10, 7, 12, 8, 30]
+      data_dir: ../velosearaptor/tests/data/24606000.000
+      editparams:
+        maskbins: [16, 17, 18]
+        max_e: 0.2
+      meta_data:
+        nominal_depth: 1157m
+    SN24607:
+      driftparams:
+        end_adcp: [2021, 10, 7, 12, 7, 12]
+        end_pc: [2021, 10, 7, 12, 8, 30]
+      dgridparams:
+        dtop: 1000
+  # Instrument-specific parameters overview
+    #SNxxxx:
+    #  data_dir:
+    #  driftparams:
+    #    end_adcp:
+    #    end_pc:
+    #  editparams:
+    #    maskbins:
+    #    max_e:
+    #    max_e_deviation:
+    #    min_correlation:
+    #    pg_limit:
+    #  dgridparams:
+    #    dtop:
+    #    dbot:
+    #    d_interval:
+    #  tgridparams:
+    #    dt_hours:
+    #    t0:
+    #    t1:
+    #    burst_average:
+    #  meta_data:
+

setup.cfg
@@ -4,3 +4,13 @@ test = pytest
 [tool:pytest]
 testpaths = velosearaptor/tests
 addopts = --ignore=setup.py
+
+[isort]
+profile = black
+
+[flake8]
+ignore =
+	E203, E402
+exclude =
+	.eggs
+max-line-length = 88

velosearaptor/io.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Module velosearaptor.io with in/out functions. Mostly provides wrapper functions to UHs `Multiread`."""
+"""Module velosearaptor.io with in/out functions."""
 
 import datetime
+from pathlib import Path
 
 import numpy as np
 import xarray as xr
+import yaml
 from pycurrents.adcp.rdiraw import Multiread, extract_raw
 
 
@@ -161,7 +163,7 @@ def extract_raw_rdi(file, i0, i1, outfile, inst="wh"):
     inst : str
         One of ('wh','os','bb','ec'). Defaults to 'wh'.
     """
-    data = extract_raw(file, inst, i0, i1, outfile=outfile)
+    _ = extract_raw(file, inst, i0, i1, outfile=outfile)
 
 
 def yday0_to_datetime64(baseyear, yday):
@@ -185,6 +187,101 @@ def yday0_to_datetime64(baseyear, yday):
     # convert to numpy datetime64
     time64 = np.array([np.datetime64(ti, "ms") for ti in time])
     return time64
+
+
+def parse_yaml_input(yamlfile, mooring, sn):
+    """Read yaml settings file and parse for a specific instrument.
+
+    Returns will be of type `None` if not present in the yml parameter file.
+
+    Parameters
+    ----------
+    yamlfile : pathlib.Path() or str
+        Parameter file.
+    mooring : str
+        Mooring ID as used in the parameter file.
+    sn : int
+        Instrument serial number as used in the parameter file, without prepended SN.
+
+    Returns
+    -------
+    Dictionary with the following entries:
+    meta_data : dict
+        General meta data for the dataset.
+    dgridparams : dict
+        Depth gridding parameters.
+    tgridparams : dict
+        Time gridding parameters.
+    editparams : dict
+        Editing parameters.
+    driftparams : dict
+        Clock drift parameters.
+    data_dir : pathlib.Path
+        ADCP data directory
+    """
+    with open(yamlfile, "r") as file:
+        yp = yaml.safe_load(file)
+
+    try:
+        _ = yp["mooring"][mooring]
+    except KeyError:
+        print(f"Mooring {mooring} not found in .yml parameter file")
+        raise
+
+    snstr = f"SN{sn}"
+    try:
+        _ = yp["mooring"][mooring][snstr]
+    except KeyError:
+        print(f"Serial numer {sn} not found in .yml parameter file")
+        raise
+
+    # global meta-data
+    meta_data = yp["meta_data"]
+    meta_data["mooring"] = mooring
+    for ll in ["lon", "lat"]:
+        meta_data[ll] = yp["mooring"][mooring][ll]
+    meta_data["sn"] = sn
+    out = dict(meta_data=meta_data)
+
+    # global processing parameters
+    pp = yp["processing_parameters"]
+    #   initialize output variables as None types in case they are not provided
+    #   in the yaml file
+    out_vars = [
+        "dgridparams",
+        "tgridparams",
+        "editparams",
+        "driftparams",
+    ]
+    for var in out_vars:
+        out[var] = None
+    for var in out_vars:
+        if var in pp:
+            out[var] = pp[var]
+
+    # instrument-specific parameters
+    d = yp["mooring"][mooring][snstr]
+    for var in ["driftparams"]:
+        if var in d:
+            out[var] = d[var]
+    if "data_dir" in d:
+        data_dir = Path(d["data_dir"])
+    else:
+        data_dir = None
+    # update global parameters with instrument-specific parameters
+    for param in ["editparams", "tgridparams", "dgridparams"]:
+        if param in d:
+            if out[param] is None:
+                out[param] = dict()
+            for k, v in d[param].items():
+                out[param][k] = v
+    if "meta_data" in d:
+        for k, v in d["meta_data"].items():
+            out["meta_data"][k] = v
+
+    out["data_dir"] = data_dir
+
+    return out
 
 
 def _is_number(s):

commit 300547814ca03881e5b33275470cbd5dceaae022

insert mooring and sn variables (#47)
Fixes a bug that was introduced in #46.

velosearaptor/madcp.py
@@ -1604,7 +1604,7 @@ class ProcessADCPyml(ProcessADCP):
     [`notebooks/parameters.yml`](https://github.com/modscripps/velosearaptor/tree/main/notebooks/parameters.yml)"""
 
     def __init__(self, parameter_file, mooring, sn, **kwargs):
-        p = io.parse_yaml_input(parameter_file, "MAVS2", 24606)
+        p = io.parse_yaml_input(parameter_file, mooring, sn)
         super().__init__(
             p["data_dir"],
             meta_data=p["meta_data"],